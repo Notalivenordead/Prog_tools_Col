0b0799ab8433069e2c885a3bc9f9870a9f67479c
Babadzakkkich <Bogdamirkobz@gmail.com>
2025-09-24 15:32:58 +0300

    Account and service created

--- projects/bank_acccont/src/main/java/com/bank/BankAccount.java
+++ projects/bank_acccont/src/main/java/com/bank/BankAccount.java
@@ -1,1 +1,77 @@
-// Реализация
+import java.util.Date;
+
+public class Account {
+    private int id;
+    private double balance;
+    private double annualInterestRate;
+    private Date dateCreated;
+
+    // Безаргументный конструктор
+    public Account() {
+        this.id = 0;
+        this.balance = 0;
+        this.annualInterestRate = 0;
+        this.dateCreated = new Date();
+    }
+
+    // Конструктор с id и балансом
+    public Account(int id, double balance) {
+        this.id = id;
+        this.balance = balance;
+        this.annualInterestRate = 0;
+        this.dateCreated = new Date();
+    }
+
+    // Геттеры и сеттеры
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    // Ежемесячный процент
+    public double getMonthlyInterest() {
+        double monthlyRate = annualInterestRate / 100 / 12;
+        return balance * monthlyRate;
+    }
+
+    // Снятие денег
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Недостаточно средств или неверная сумма.");
+        }
+    }
+
+    // Пополнение счета
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            System.out.println("Сумма должна быть положительной.");
+        }
+    }
+}
